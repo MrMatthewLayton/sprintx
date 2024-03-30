c87f94db4b2224cc3e47e54f2a7366cc4c0ff2f0
Matthew Layton <matthew.layton@live.co.uk>
2024-03-30 00:49:29 +0000

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {ConfigComponent} from "./components/config/config.component";
-import {WorkoutComponent} from "./components/workout/workout.component";
-
-export const routes: Routes = [
-  {path: "config", component: ConfigComponent},
-  {path: "workout", component: WorkoutComponent},
-  {path: "**", redirectTo: "/config"}
-];
